cddba580983e22a5e61df82e38c6eae5619213b7
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-08-27 18:05:43 -0400

    Assemblage des views couchdb + script pour tout charger

--- roulerJobETL.sh
+++ roulerJobETL.sh
@@ -1,3 +1,10 @@
+
+# Création de la database couch et des views
+curl -X PUT http://localhost:5984/parking_api
+# Creer les views couchdb
+curl -X PUT http://localhost:5984/parking_api/_design/nodejs -d @couchdb_stuff/design_views.json
+
+
 # créer les folder
 mkdir out
 mkdir out/data
@@ -5,4 +12,5 @@ mkdir out/data
 # Caller le job pour ajouter un type de source.
 node ajouterTypeADocumentFeatures.js data/PANNEAU_S.GEOJSON PANNEAU_S
 
-
+# Chargement de chacune des features.
+node loadDocumentFeaturesAsPoints.js out/data/PANNEAU_S.GEOJSON
