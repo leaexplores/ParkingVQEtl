a14d4d06bc0018d97639531b67f1b8cb529d3d59
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-08-28 00:19:10 -0400

    Job etl qui traite de tous les fichiers pour parking api

--- roulerJobETL.sh
+++ roulerJobETL.sh
@@ -11,9 +11,15 @@ mkdir out/data
 
 # Caller le job pour ajouter un type de source.
 node ajouterTypeADocumentFeatures.js data/PANNEAU_S.GEOJSON PANNEAU_S
+node ajouterTypeADocumentFeatures.js data/PARCOMETRE.GEOJSON PARCOMETRE
+
+# Conversion en geojson + Ajout type source pour bornes fontaines.
+node loadCSVBornesFontainesToGeojson.js data/BORNE.csv
 
 # Chargement de chacune des features.
 node loadDocumentFeaturesAsPoints.js out/data/PANNEAU_S.GEOJSON
+node loadDocumentFeaturesAsPoints.js out/data/PARCOMETRE.GEOJSON
+node loadDocumentFeaturesAsPoints.js out/data/BORNE.csv
 
 # Démarage de la génération de la génération de la vue.
 curl -X GET http://localhost:5984/parking_api/_design/nodejs/_view/keys?limit=0
