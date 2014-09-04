061c4bb9c1ced62a4d5965a4aa4ef96378dfe90e
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-09-04 01:28:14 -0400

    Updated bash script

--- roulerJobETL.sh
+++ roulerJobETL.sh
@@ -5,9 +5,10 @@ curl -X PUT http://localhost:5984/parking_api
 curl -X PUT http://localhost:5984/parking_api/_design/nodejs -d @couchdb_stuff/design_views.json
 
 
-# créer les folder
+# créer les folder et tenter de vider si ca existe deja....
 mkdir out
 mkdir out/data
+rm out/data/*
 
 # Caller le job pour ajouter un type de source.
 node ajouterTypeADocumentFeatures.js data/PANNEAU_S.GEOJSON PANNEAU_S
@@ -16,8 +17,12 @@ node ajouterTypeADocumentFeatures.js data/PARCOMETRE.GEOJSON PARCOMETRE
 # Conversion en geojson + Ajout type source pour bornes fontaines.
 node loadCSVBornesFontainesToGeojson.js data/BORNE.csv
 
+# Transformer PANNEAU_S sans les dumb panneau + nouvelles props :)
+node epurerPanneau_S.js out/data/PANNEAU_S.GEOJSON out/data/PANNEAU_S_TRANSFO.GEOJSON
+
+
 # Chargement de chacune des features.
-node loadDocumentFeaturesAsPoints.js out/data/PANNEAU_S.GEOJSON
+node loadDocumentFeaturesAsPoints.js out/data/PANNEAU_S_TRANSFO.GEOJSON
 node loadDocumentFeaturesAsPoints.js out/data/PARCOMETRE.GEOJSON
 node loadDocumentFeaturesAsPoints.js out/data/BORNE.csv
 
